1a685deedd0ddcba3cdad2246cd8932905e9686a
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2023-12-18 21:54:19 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int n = 925, isPrime=1;
+    for (int i = 2; i < n; i++)
+    {
+        if(n%i == 0){
+            isPrime = 0;
+            break;
+        }
+    }
+    if(isPrime){
+        printf("The number is prime\n");
+    }
+    else{
+        printf("The number is not prime\n");
+    }
+     
+    return 0;
+}
+// n value used as 5,6,9,2,4,8,22,24
