50f232b2f334ae687c45854b374bfcd187c21820
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2023-12-19 06:13:41 +0530

    Create funtions1.c

---
+++ funtions1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void display();//funtion prototype
+
+int main ()
+{
+  int a;
+  display(); //funtion call
+  return 0;
+}
+void display(){//display defination
+ printf("good morning\n");
+ printf("good afternoon\n");
+ printf("good evening\n");
+}
