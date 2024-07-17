234c117ceff6e3b475839c064171c1cf66d2711c
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-07-17 21:52:54 +0530

    Create 2sum.c

---
+++ 2sum.c
@@ -0,0 +1,13 @@
+//Write a program which does the following
+//Output the sum of 3 and 4 using printf.
+//Output the sum of 1 and 2 using printf, but on a new line.
+
+#include <stdio.h>
+
+int main() {
+    
+    // add %d along with \n to print on two different lines
+	printf("%d\n", 3 + 4);
+	printf("%d\n", 1 + 2);
+}
+
