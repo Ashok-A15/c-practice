b12b35112366698c525be56a937cca39dfb50fe4
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-07-17 21:51:29 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+//Write a program which does the following
+//Add a printf statement and output the sum of 3 + 4
+//Add another printf statement and output the sum of 2 + 1.
+//Use the printf syntax we learned in last problem.
+//Note - Notice that in the output they are printed together without any space between them.
+
+
+#include <stdio.h>
+
+int main() {
+	// add two printf statements here
+	printf("%d", 3+4);
+	printf("%d", 2+1);
+}
